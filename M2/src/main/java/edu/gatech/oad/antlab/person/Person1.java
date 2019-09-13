edb17a418aa29d5d469358488f17e0ed15309b3c
robin luo <robin@lawn-128-61-24-67.lawn.gatech.edu>
2019-09-13 16:14:55 -0400

    robin upload

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return input.substring(input.length - 2) + input.substring(0, input.length - 2);
+	  return input.substring(input.length() - 2) + input.substring(0, input.length() - 2);
 	}
 	
 	/**
