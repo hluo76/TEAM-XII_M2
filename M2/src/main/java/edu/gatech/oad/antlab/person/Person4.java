a4be6f2e627c3a52bc0434f6c9184f0bd13cad1b
sherrychen <sherryq1125@gmail.com>
2019-09-13 16:43:02 -0400

    person4

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -4,47 +4,54 @@ package edu.gatech.oad.antlab.person;
  *  A simple class for person 4
  *  returns their name and a
  *  modified string 
- *  
- *  @author Bob
+ *
+ *  @author Qianyun Chen
  *  @version 1.1
  */
 public class Person4 {
   /** Holds the persons real name */
   private String name;
-    /**
-     * The constructor, takes in the persons
-     * name
-     * @param pname the person's real name
-     */
+  /**
+   * The constructor, takes in the persons
+   * name
+   * @param pname the person's real name
+   */
   public Person4(String pname) {
     name = pname;
   }
-    /**
-     * This method should return a string
-     * where each character is 1 greater 
-     * than its previous value.  So
-     * given "abc123" it should return
-     * "bcd234".
-     *
-     * @param input the string to be modified
-     * @return the modified string
-     */
-    private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+  /**
+   * This method should return a string
+   * where each character is 1 greater
+   * than its previous value.  So
+   * given "abc123" it should return
+   * "bcd234".
+   *
+   * @param input the string to be modified
+   * @return the modified string
+   */
+  private String calc(String input) {
+    int A = 0;
+    String a = "";
+    //Person 4 put your implementation here
+    for(int i = 0; i < input.length(); i++) {
+      A = (int)input.charAt(i);
+      A++;
+      a = (char)A+"";
     }
-    
-    /**
-     * Return a string rep of this object
-     * that varies with an input string
-     *
-     * @param input the varying string
-     * @return the string representing the 
-     *         object
-     */
-    public String toString(String input) {
-      return name + calc(input);
-    }
+    return a;
+  }
+
+  /**
+   * Return a string rep of this object
+   * that varies with an input string
+   *
+   * @param input the varying string
+   * @return the string representing the
+   *         object
+   */
+  public String toString(String input) {
+
+    return name + calc(input);
+  }
 
 }
-
