6289a4bebc239c0f82fb68a0b43082a76572da96
robin <robin@lawn-128-61-29-194.lawn.gatech.edu>
2019-09-16 16:42:19 -0400

    M3 update

--- M3/src/DemoApp/main.py
+++ M3/src/DemoApp/main.py
@@ -2,6 +2,9 @@ from DemoApp import app
 from flask import render_template, url_for, flash, Markup, redirect, request, make_response
 from DemoApp.forms import *
 
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -15,8 +18,8 @@ def load():
     if loadform.search.data and loadform.validate_on_submit():
         name = loadform.name.data
         type = loadform.type.data
-        return render_template("game.html")
-    return render_template("load.html")
+        return render_template("game.html", form = loadform)
+    return render_template("load.html",  form=loadform)
 
 
 @app.route("/game", methods=['GET', 'POST'])
